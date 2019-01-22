cbe3b9423a94fc73cd396feb5468745c94ac9f5c
zhuxk <zhuxk@staff.chinabyte.com>
2019-01-22 10:57:00 +0800

    创建项目

---
+++ src/main/java/com/zxk/elasticsearch/Main.java
@@ -0,0 +1,10 @@
+package com.zxk.elasticsearch;
+
+/**
+ * @Description:
+ * @Author: zhuxiaokang
+ * @CreateDate: 2019/1/22 10:54
+ * @Version: 1.0
+ */
+public class Main {
+}
---
+++ src/test/java/com/zxk/elasticsearch/MainTest.java
@@ -0,0 +1,10 @@
+package com.zxk.elasticsearch;
+
+/**
+ * @Description:
+ * @Author: zhuxiaokang
+ * @CreateDate: 2019/1/22 10:56
+ * @Version: 1.0
+ */
+public class MainTest {
+}
